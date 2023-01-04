37811d39b3cf9c61e0b1ce086f9cda2b539f0920
MCA <you@example.com>
2023-01-04 10:59:20 +0530

    Initial commit

---
+++ circle.py
@@ -0,0 +1,6 @@
+import math
+def area(r):
+    return math.pi*r*r
+def perimeter(r):
+    return math.pi*2*r
+package 3D_Graphics:
---
+++ rectangle.py
@@ -0,0 +1,5 @@
+def area(h,w):
+    return h*w
+def perimeter(h,w):
+    return 2*(h+w)
+
